c2c8b23329ea549d5ecf214a4d013c36d239c77d
Adiks <apacholak97@interia.pl>
2023-02-28 09:36:19 +0100

    configure swagger request validator

--- src/test/java/com/adrian/spring6restmvc/controller/BeerControllerRestAssuredTest.java
+++ src/test/java/com/adrian/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -1,5 +1,7 @@
 package com.adrian.spring6restmvc.controller;
 
+import com.atlassian.oai.validator.OpenApiInteractionValidator;
+import com.atlassian.oai.validator.restassured.OpenApiValidationFilter;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,6 +27,9 @@ public class BeerControllerRestAssuredTest {
     @LocalServerPort
     Integer localPort;
 
+    OpenApiValidationFilter filter = new OpenApiValidationFilter(OpenApiInteractionValidator
+            .createForSpecificationUrl("oa3.yml").build());
+
     @BeforeEach
     void setUp() {
         RestAssured.baseURI = "http://localhost";
@@ -35,6 +40,7 @@ public class BeerControllerRestAssuredTest {
     void testListBeers() {
         given().contentType(ContentType.JSON)
                 .when()
+                .filter(filter)
                 .get("/api/v1/beer")
                 .then()
                 .assertThat().statusCode(200);
